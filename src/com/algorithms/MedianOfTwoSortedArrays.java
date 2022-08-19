44c8658fd846a144266abc40df2890c5b5f15fd2
Tabish Matin <tabishmatin1@gmail.com>
2022-08-19 23:21:39 +0530

    Updated MedianOfTwoSortedArays.java

--- src/com/algorithms/MedianOfTwoSortedArrays.java
+++ src/com/algorithms/MedianOfTwoSortedArrays.java
@@ -3,6 +3,10 @@ package com.algorithms;
 import java.util.ArrayList;
 import java.util.Collections;
 
+/* Given two sorted arrays nums1 and nums2 of size m and n respectively, 
+ * return the median of the two sorted arrays.
+ */
+
 public class MedianOfTwoSortedArrays {
 
 	public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
